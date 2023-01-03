926f787898d9cd80e944a31df344b81119ed89b1
Dell <praweenjha21@gmail.com>
2023-01-03 21:07:19 +0530

    Added some methods in my local branch

--- src/Testing/TestSample1.java
+++ src/Testing/TestSample1.java
@@ -2,6 +2,20 @@ package Testing;
 
 public class TestSample1 {
 
+	
+	String mytest="pass";
+	
+	public void test()
+	{
+		if(mytest.contains("pa"))
+		{
+			System.out.println("test passed");
+		}
+		else
+		{
+			System.out.println("test failed");
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("test sample");
 
